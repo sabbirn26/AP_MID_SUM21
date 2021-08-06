4ed93d9005201f7269299ba8a5e253009d246a45
Sabbir Nasir <18-36044-1@student.aiub.edu>
2021-08-06 23:29:09 +0600

    assignment updated done

---
+++ Web API/BLL/DepartmentService.cs
@@ -0,0 +1,44 @@
+﻿using DAL;
+using BEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class DepartmentService
+    {
+        public static List<string> GetDepartmentNames()
+        {
+            return DepartmentRepo.GetDepartmentNames();
+        }
+
+        public static List<DepartmentModel> GetDepartments()
+        {
+            var departments = DepartmentRepo.GetDepartments();
+            List<DepartmentModel> departmentList = new List<DepartmentModel>();
+            foreach (var department in departments)
+            {
+                var d = new DepartmentModel()
+                {
+                    ID = department.ID,
+                    Name = department.Name
+                };
+                departmentList.Add(d);  
+            }
+            return departmentList;
+        }
+
+        public static void AddDepartment(DepartmentModel dept)
+        {
+            var d = new Department()
+            {
+                ID = dept.ID,
+                Name = dept.Name
+            };
+            DepartmentRepo.AddDepartment(d);
+        }
+    }
+}
---
+++ Web API/Web API/Controllers/DepartmentController.cs
@@ -0,0 +1,35 @@
+﻿using BLL;
+using BEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_API.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        [Route("api/Department/Names")]
+        [HttpGet]
+        public List<String> GetNames()
+        {
+            return DepartmentService.GetDepartmentNames();
+        }
+
+        [Route("api/Department/GetAll")]
+        [HttpGet]
+        public List<DepartmentModel> GetAllDepartments()
+        {
+            return DepartmentService.GetDepartments();
+        }
+
+        [Route("api/Department/Add")]
+        [HttpPost]
+        public void Add(DepartmentModel dept)
+        {
+            DepartmentService.AddDepartment(dept);
+        }
+    }
+}
